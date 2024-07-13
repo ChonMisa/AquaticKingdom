743d25c67e04532d612a83cab282e71678211c27
Your Name <you@example.com>
2024-07-13 23:01:36 +0600

    first commit

---
+++ apps/fish/api/urls.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import DefaultRouter
+
+from apps.fish.api.views import FishViewSet
+
+
+router = DefaultRouter()
+
+router.register('fish', FishViewSet)
+
+urlpatterns = router.urls
+
+
